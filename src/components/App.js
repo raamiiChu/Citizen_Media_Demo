18d828421eaae4b58e26bb53ea67b07d610f390e
raamiiChu <87169493+raamiiChu@users.noreply.github.com>
2022-11-29 22:03:27 +0800

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Title from "./Title/Title"
+import Carousel from "./Carousel/Carousel.js";
+
+
+const App = () => {
+  return (
+    <div>
+      <Title />
+      <Carousel />
+    </div>
+  );
+};
+
+export default App;
